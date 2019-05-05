e429cf20e7e19b6766d655e7fdd95a6a8702620b
Gian Libunao <gianlibunao@gmail.com>
2019-05-05 20:15:42 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,6 +13,8 @@ def age_appropriate_birthday(birthday_kids)
 birthday_kids.each { |x,y| 
 if y < 12
 puts "Happy Birthday #{x}! You are now #{y} years old!"
+else
+puts "You are too old for this."
 end
 }
 end
