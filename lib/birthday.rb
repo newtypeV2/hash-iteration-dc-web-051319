6758db5bc6f41c4dee90fb1f6f50356cdeee15b8
Gian Libunao <gianlibunao@gmail.com>
2019-05-05 20:06:53 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  birthday_kids each { |x,y| puts "Happy Birthday #{x}! You are now #{x}years old!"
+  birthday_kids each { |x,y| puts "Happy Birthday #{x}! You are now #{x}years old!"}
 end
 
 
